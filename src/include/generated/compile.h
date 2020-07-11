90c70230574eb976fdac9dacd63cab7a5ec8d53d
Guoqing Li <li-guoqing@163.com>
2020-07-11 09:43:02 +0800

    .gitignore:remove generated file compile.h and ignore generate files for git

--- src/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 103 */
-/* SMP */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#103 SMP Fri Jul 3 10:31:38 CST 2020"
-#define LINUX_COMPILE_BY "root"
-#define LINUX_COMPILE_HOST "ThinkPad-T440"
-#define LINUX_COMPILER "gcc version 5.3.1 20160113 (Linaro GCC 5.3-2016.02) "
